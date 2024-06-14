96e966d3c8e68e6453771e78aee57bf6a6373f52
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-06-14 15:59:33 +0700

    commit Scores

--- Scores.jsx
+++ Scores.jsx
@@ -1,32 +1,38 @@
-import React from "react";
 import "../index.css";
-export default function Statistic({ sourceResults }) {
-    const averageScore = Math.floor(
-        sourceResults.reduce((sum, result) => sum + result.score, 0) /
-        sourceResults.length
-    );
-    const maxScore = sourceResults.reduce(
-        (max, result) => (result.score > max ? result.score : max),
-        0
-    );
-    const minScore = sourceResults.reduce(
-        (min, result) => (result.score < min ? result.score : min),
-        100
-    );
+import Statistic from "./Statistic"
+export default function Scores({ courseName, sourceResults }) {
+
+
     return (
         <>
-            <div class="statistic">
-                <tr>
-                    <td class="statistic_title">AVERAGE</td>
-                    <td class="statistic_title">MAX</td>
-                    <td class="statistic_title">MIN</td>
-                </tr>
-                <tr>
-                    <td class="statistic_value">{averageScore}</td>
-                    <td class="statistic_value">{maxScore}</td>
-                    <td class="statistic_value">{minScore}</td>
-                </tr>
+            <div class="scores">
+                <h1>{courseName}</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sourceResults.map((result) => (
+                                <tr>
+                                    <td>{result.firstName}</td>
+                                    <td>{result.lastName}</td>
+                                    <td className={result.score < 50 ? 'warning' : ''}>{result.score}</td>
+                                </tr>
+                            ))
+                        }
+
+
+
+                    </tbody>
+
+                </table>
+                <Statistic sourceResults={sourceResults} />
             </div>
         </>
-    );
+    )
 }
